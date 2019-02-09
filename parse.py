212a5185a559450c5cf010627560a45464655bb9
ajt232 <ajt232@cornell.edu>
2019-02-09 15:52:52 -0500

    changing check from Return to Str node

--- parse.py
+++ parse.py
@@ -1,17 +1,13 @@
 import ast
 
+### Does this string contain hello world?
 def findHello():
     with open('hello.py') as f:
         content = f.read()
 
     code = ast.parse(content)
     for node in ast.walk(code):
-        if isinstance(node, ast.Return) and "Hello, World!" == node.value.s:
+        if isinstance(node, ast.Str) and "Hello, World!" == node.s:
             return True
 
     return False
-
-print(findHello())
-
-
-# exec(compile(code, filename="", mode="exec"))
